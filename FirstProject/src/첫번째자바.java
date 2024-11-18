2299c159bd1f4220941a99e384cf7d31e795dad2
SIST-112 <SIST-112@DESKTOP-MAT3KCP>
2024-11-18 14:26:26 +0900

    11

---
+++ FirstProject/src/첫번째자바.java
@@ -0,0 +1,65 @@
+/*
+		1. 대소문자 구분
+		2. 들여쓰기 => 가독성
+		3. 명령뮨이 종료 반드시 ;
+		   ; => 파이썬 / Kotlin / javascript (;을 사용하는 언어)
+		4. 자바는 { 
+		5. 문법
+		   데이터 저장 ======> 데이터 처리(요청) =====> 화면 출력
+		   	  |                   |                 |
+		   	  |              연산자 / 제어뮨         명령프롬프트 : Application
+		   	  |                                     문법
+		   	  |             =============        브라우저 출력 : Web Application
+		   	  |                                     주력
+	   	      |             	  |              모바일 출력 : Mobile Application
+	   	      |             	메소드              Spring : Java / Kotlin
+		   한개 저장 (메모리) : 변수/상수
+		   여러개 저장 : 배열
+		              클래스
+		   -------------------------------------
+		       프로그램 종료시에 사라진다
+		   파일 / 오라클  => 영구적인 저장
+		   
+		   =========================> 기본 문법
+		   1. 기능을 모아서 관리
+		      ===
+		        => 단락 : 메소드
+		        => 클래스 / 객체 => 객체지향 프로그램
+		        => 프로그램 유지 (예외처리)
+		        =============================== 8장
+		        => 라이브러리 : 자바에서 지원
+		        => 1차 응용프로그램
+		   =============> Web
+		   1. HTML : 브라우저에서 실행
+		             자바는 브러우저에서는 일반 텍스트
+		      => 투박 =>  CSS / 자바스크립트
+		   2. JSP (요청 => HTML => Java)
+		           응답 => Java => HTML
+		        |  MVC
+		          =====
+		          Front / Back
+		            |       | 자바 / 오라클 / Spring
+		         JS / TS
+                 ==================================+ Full Stack
+           3. 통합 : Framework : Spring
+                    기본 틀 => 메인 보드
+		   4. 데이터 분석 :  Python / Numpy / Pandas
+		   				  MatplotLib
+		   5. NoSql => elasticsearch
+		   					| 오라클 역할
+		   6. 배포 : AWS
+		      5.5 개월
+		   =====================================================
+		   Spring-Boot / JPA / MySQL / React-Query
+		   Redux / Vue3 / NextJS / TypeScript / Node JS
+*/
+public class 첫번째자바 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+		System.out.println("박현수");
+		System.out.println("Hello");
+	}
+
+}
