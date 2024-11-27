83719a3cf49be77d83e7bdf934e74c275b79d57c
SIST-112 <SIST-112@DESKTOP-MAT3KCP>
2024-11-27 17:45:15 +0900

    2024-11-27

--- Chapter03_제어문_반복문_3/src/반복문_예제.java
+++ Chapter03_제어문_반복문_3/src/반복문_예제.java
@@ -159,18 +159,7 @@ public class 반복문_예제 {
 			cb++;
 			System.out.println(" ");
 		}
-		//
-		for(int i=1;i<=5;i++)
-		{
-			for(int j=1;j<=5;j++)
-			{
-				if(6-i==j)
-					System.out.print("★");
-				else
-					System.out.print("☆");
-			}
-			System.out.println();
-		}
+		
 	}
 
 }
